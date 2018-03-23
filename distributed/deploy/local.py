65ae6882715782e88af57a95d5465f2ff16e77c0
Matti Lyra <matti.lyra@comtravo.com>
2018-03-23 18:13:21 +0100

    Trying to fix issue with non existent tasks in unkown_durations[prefix]

--- distributed/deploy/local.py
+++ distributed/deploy/local.py
@@ -264,7 +264,7 @@ class LocalCluster(object):
 
         This can be implemented either as a function or as a Tornado coroutine.
         """
-        logging.getLogger(__name__).info('Adding %s workers. %s desired, %s currently', n - len(self.workers), n, len(self.workers))
+        logging.getLogger(__name__).info('Adding %s workers. %s desired, %s/%s currently', n - len(self.workers), n, len(self.scheduler.workers), len(self.workers))
         yield [self._start_worker(**kwargs)
                for i in range(n - len(self.workers))]
 
